e62776958e3e57f3ca9996f80f51ab2852286bd6
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2023-10-21 15:54:13 -0300

    feat: initial commit

---
+++ src/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Data;
+using System.Diagnostics;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    var processesToKill = new string[]{
+      "OVRRedir",
+      "OVRServer_x64",
+      "OVRServiceLauncher",
+      "PnkBstrA",
+    };
+
+    var processMap = new Dictionary<string, List<Process>>();
+
+    foreach (var process in Process.GetProcesses())
+    {
+      if (!processMap.ContainsKey(process.ProcessName))
+      {
+        processMap[process.ProcessName] = new List<Process>();
+      }
+
+      processMap[process.ProcessName].Add(process);
+    }
+
+
+    Console.WriteLine($"Current running processes ({processMap.Keys.Count}):");
+    foreach (var processName in processMap.Keys.OrderBy(x => x).ToArray())
+    {
+      Console.WriteLine($"  - {processName}");
+    }
+
+    var killedProcesses = new List<string>();
+
+    foreach (var entry in processMap)
+    {
+      if (processesToKill.Contains(entry.Key))
+      {
+        foreach (var process in entry.Value)
+        {
+          process.Kill();
+
+          killedProcesses.Add(entry.Key);
+        }
+      }
+    }
+
+    Console.WriteLine($"Killed processes ({killedProcesses.Count}):");
+    foreach (var processName in killedProcesses)
+    {
+      Console.WriteLine($"  - {processName}");
+    }
+  }
+}
